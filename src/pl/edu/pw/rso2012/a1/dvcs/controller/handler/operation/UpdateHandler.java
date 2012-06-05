97fc56904f411e3f4bc87694fbd98d988c653aab
amakarew <amakarewicz@gmail.com>
2012-06-05 19:57:51 +0200

    catch for NoSuchCommitException

--- src/pl/edu/pw/rso2012/a1/dvcs/controller/handler/operation/UpdateHandler.java
+++ src/pl/edu/pw/rso2012/a1/dvcs/controller/handler/operation/UpdateHandler.java
@@ -12,6 +12,7 @@ import pl.edu.pw.rso2012.a1.dvcs.controller.event.view.ShowErrorEvent;
 import pl.edu.pw.rso2012.a1.dvcs.controller.exception.HandlerNotImplementedException;
 import pl.edu.pw.rso2012.a1.dvcs.controller.handler.ApplicationHandler;
 import pl.edu.pw.rso2012.a1.dvcs.model.communication.Commit;
+import pl.edu.pw.rso2012.a1.dvcs.model.communication.exception.NoSuchCommitException;
 
 public class UpdateHandler extends ApplicationHandler {
 	public UpdateHandler(final Controller controller) {
@@ -39,6 +40,12 @@ public class UpdateHandler extends ApplicationHandler {
 		{
 			controller.onImportantEvent(new UpdateCompleteEvent());
 			controller.onImportantEvent(new ShowErrorEvent("Wystąpił nieoczekiwany błąd, kod: 2"));
+		}
+		catch (NoSuchCommitException e)
+		{
+			controller.onImportantEvent(new UpdateCompleteEvent());
+			controller.onImportantEvent(new ShowErrorEvent("Nie ma takiej rewizji"));
+		
 		}
 	}
 	
